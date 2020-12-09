05fbf19454379f100b834c5f9a4841c64ed9f3f4
anandkushwaha064 <anandkushwaha064@gmail.com>
2020-12-09 17:17:11 +0530

    firstcommit

---
+++ CreateGit.bat
@@ -0,0 +1,13 @@
+git init
+git config --global user.name "anandkushwaha064"
+git config --global user.email "anandkushwaha064@gmail.com"
+git status
+git add .
+git commit -m "firstcommit"
+
+git remote add origin https://github.com/anandkushwaha064/Practics.git
+git push -u origin master
+
+
+
+start chrome https://github.com/anandkushwaha064
